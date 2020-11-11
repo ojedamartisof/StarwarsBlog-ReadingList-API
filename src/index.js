270e6dec97f2d3da567dfca889486f8c5fc3791c
ojedamartisof <ojedamartinasofia@gmail.com>
2020-11-11 17:06:09 -0300

    git commit -m

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css'; 
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import { ContextWrapper } from './store/appContext';
+import Layout from './layout';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <ContextWrapper>
+        <Layout />
+    </ContextWrapper>
+    ,
+    document.getElementById('root'));
